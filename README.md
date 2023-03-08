930840521d479d8d0bb8b620e023d12f2dce5e5e
william Silva <wkoju84@gmail.com>
2023-03-08 12:44:56 -0300

    Adicionando o readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#Colocando em prática conceitos de Programação Orientada a Objetos na Linguagem Java
+
+- abstração e encapsulamento
+- Herança e polimorfismo
+- Concluindo a criação de classes do domínio
+- Interligando as classes de domínio
---
+++ mesada_sobrinho/src/Program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        int A = 30;
+        int B = 10;
+
+        int SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+    }
+}
